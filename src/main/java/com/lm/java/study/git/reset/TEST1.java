b50c0935c0033f54bb884a7ae9615d842ad0c928
limao <limao@meituan.com>
2021-07-11 12:41:44 +0800

    版本1

---
+++ src/main/java/com/lm/java/study/git/reset/TEST1.java
@@ -0,0 +1,14 @@
+package com.lm.java.study.git.reset;
+
+/**
+ * @author lm
+ * @version 1.0
+ * @desc TEST
+ * @created 2021/7/11 下午12:38
+ **/
+public class TEST1 {
+    public static void main(String[] args) {
+        System.out.println("版本1");
+    }
+
+}
