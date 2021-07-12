8eaee334b4f4a393a2156f590e776691d6154b2f
limao <limao@meituan.com>
2021-07-12 23:37:39 +0800

    no message

--- src/main/java/com/lm/java/study/git/revert/test.java
+++ src/main/java/com/lm/java/study/git/revert/test.java
@@ -11,6 +11,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3);
         System.out.println(4);
     }
 }
